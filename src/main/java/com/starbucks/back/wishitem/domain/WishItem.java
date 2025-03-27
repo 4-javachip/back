0fd6d5db8f97eca979e23e5a01d7af2ebdb38bd1
JungDongSeop <ds123911@gmail.com>
2025-03-27 21:04:01 +0900

    feat: wish_list 엔티티 수정

--- src/main/java/com/starbucks/back/wishitem/domain/WishItem.java
+++ src/main/java/com/starbucks/back/wishitem/domain/WishItem.java
@@ -1,13 +1,15 @@
 package com.starbucks.back.wishitem.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "wish_item")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WishItem {
 
     @Id
@@ -22,4 +24,17 @@ public class WishItem {
 
     @Column(name = "product_uuid", nullable = false, length = 50)
     private String productUuid;
+
+    @Builder
+    public WishItem(
+            Long id,
+            String wishItemUuid,
+            String userUuid,
+            String productUuid
+    ) {
+        this.id = id;
+        this.wishItemUuid = wishItemUuid;
+        this.userUuid = userUuid;
+        this.productUuid = productUuid;
+    }
 }
