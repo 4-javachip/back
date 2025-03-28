7cba256cbf5f5a2679bd627b91c100198c7f7a98
HyeonProG <167273902+HyeonProG@users.noreply.github.com>
2025-03-28 16:39:12 +0900

    refactor: 색상 옵션 api 수정 (#63)

--- src/main/java/com/starbucks/back/option/color/presentation/ColorController.java
+++ src/main/java/com/starbucks/back/option/color/presentation/ColorController.java
@@ -1,12 +1,18 @@
 package com.starbucks.back.option.color.presentation;
 
+import com.starbucks.back.common.entity.BaseResponseEntity;
+import com.starbucks.back.common.entity.BaseResponseStatus;
 import com.starbucks.back.option.color.application.ColorService;
 import com.starbucks.back.option.color.dto.in.RequestColorDto;
 import com.starbucks.back.option.color.dto.out.ResponseColorDto;
 import com.starbucks.back.option.color.vo.in.RequestColorVo;
 import com.starbucks.back.option.color.vo.out.ResponseColorVo;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("/api/v1/option/color")
 @RestController
@@ -15,31 +21,66 @@ public class ColorController {
 
     private final ColorService colorService;
 
+    /**
+     * 색상 추가
+     * @param requestColorDto
+     */
+    @Operation(summary = "CreateColor API", description = "CreateColor API 입니다.", tags = {"Color-service"})
     @PostMapping
-    public void createColor(@RequestBody RequestColorDto requestColorDto) {
+    public BaseResponseEntity<Void> createColor(@RequestBody RequestColorDto requestColorDto) {
         colorService.addColor(requestColorDto);
+        return new BaseResponseEntity<>(BaseResponseStatus.SUCCESS);
     }
 
+    /**
+     * 색상 id로 색상 조회
+     * @param id
+     */
     @GetMapping("/{id}")
-    public ResponseColorVo getColorById(@PathVariable("id") Long id) {
+    public BaseResponseEntity<ResponseColorVo> getColorById(@PathVariable("id") Long id) {
         ResponseColorDto responseColorDto = colorService.getColorById(id);
-        return responseColorDto.toVo();
+        return new BaseResponseEntity<>(responseColorDto.toVo());
     }
 
+    /**
+     * 색상 이름으로 색상 조회
+     * @param name
+     */
     @GetMapping("/search")
-    public ResponseColorVo getColorByName(@RequestParam String name) {
+    public BaseResponseEntity<ResponseColorVo> getColorByName(@RequestParam String name) {
         ResponseColorDto responseColorDto = colorService.getColorByName(name);
-        return responseColorDto.toVo();
+        return new BaseResponseEntity<>(responseColorDto.toVo());
     }
 
+    /**
+     * 색상 전체 조회
+     */
+    @GetMapping("/list")
+    public BaseResponseEntity<List<ResponseColorVo>> getAllColors() {
+        List<ResponseColorVo> result = colorService.getAllColors().stream()
+                .map(ResponseColorDto::toVo)
+                .collect(Collectors.toList());
+        return new BaseResponseEntity<>(result);
+    }
+
+    /**
+     * 색상 수정
+     * @param requestColorVo
+     */
     @PutMapping
-    public void updateColor(@RequestBody RequestColorVo requestColorVo) {
+    public BaseResponseEntity<Void> updateColor(@RequestBody RequestColorVo requestColorVo) {
         colorService.updateColor(RequestColorDto.from(requestColorVo));
+        return new BaseResponseEntity<>(BaseResponseStatus.SUCCESS);
     }
 
+    /**
+     * 색상 삭제
+     * @param requestColorVo
+     */
     @DeleteMapping
-    public void deleteColor(@RequestBody RequestColorVo requestColorVo) {
+    public BaseResponseEntity<Void> deleteColor(@RequestBody RequestColorVo requestColorVo) {
         colorService.deleteColor(RequestColorDto.from(requestColorVo));
+        return new BaseResponseEntity<>(BaseResponseStatus.SUCCESS);
     }
 
 }
