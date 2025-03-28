aae1d7a8b88a51e84c01aca11028833b68798188
HyeonProG <167273902+HyeonProG@users.noreply.github.com>
2025-03-28 18:01:40 +0900

    feat: size option api 설계 (#64)
    * feat: 크기 옵션 dto, vo, repository 설계
    
    * feat: 사이즈 옵션 api 설계

---
+++ src/main/java/com/starbucks/back/option/size/vo/in/RequestSizeVo.java
@@ -0,0 +1,13 @@
+package com.starbucks.back.option.size.vo.in;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RequestSizeVo {
+
+    private Long id;
+    private String name;
+
+}
