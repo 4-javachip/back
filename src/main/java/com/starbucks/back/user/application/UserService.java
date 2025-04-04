12c60c5d67b9467573f7801632b87142c21b908c
KorMJ <alswh456123@kyungsung.ac.kr>
2025-04-04 12:21:09 +0900

    feat : add Jwt Provider, Filter, ApplicationConfig

--- src/main/java/com/starbucks/back/user/application/UserService.java
+++ src/main/java/com/starbucks/back/user/application/UserService.java
@@ -1,10 +1,12 @@
 package com.starbucks.back.user.application;
 
 import com.starbucks.back.user.dto.in.RequestSignUpDto;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserService {
     void signUp(RequestSignUpDto requestSignUpDto);
     boolean existsEmail(String email);
     boolean existsNickname(String nickname);
 
+    UserDetails loadUserByUsername(String userUuid);
 }
