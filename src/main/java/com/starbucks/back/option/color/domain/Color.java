e2d8841ca18bc45fec2477d1854ebaa09cd96769
HyeonProG <guswo3443@naver.com>
2025-04-07 21:43:51 +0900

    fix: 색상, 사이즈 수정

--- src/main/java/com/starbucks/back/option/color/domain/Color.java
+++ src/main/java/com/starbucks/back/option/color/domain/Color.java
@@ -1,19 +1,17 @@
 package com.starbucks.back.option.color.domain;
 
-import com.starbucks.back.common.entity.SoftDeletableEntity;
+import com.starbucks.back.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Table(name = "color_option")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Color extends SoftDeletableEntity {
+public class Color extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,7 +20,7 @@ public class Color extends SoftDeletableEntity {
     /**
      * 색상 이름
      */
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
 
     @Builder
