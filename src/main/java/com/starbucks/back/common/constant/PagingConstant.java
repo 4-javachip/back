91d99010f258710fc1d83a7e24a87081cf8179b8
HyeonProG <guswo3443@naver.com>
2025-04-08 16:19:15 +0900

    feat: pagingConstant 설계

---
+++ src/main/java/com/starbucks/back/common/constant/PagingConstant.java
@@ -0,0 +1,12 @@
+package com.starbucks.back.common.constant;
+
+public class PagingConstant {
+
+    public static final int DEFAULT_PAGE_SIZE = 20;
+    public static final int DEFAULT_PAGE_NUMBER = 0;
+
+    private PagingConstant() {
+        throw new UnsupportedOperationException("해당 클래스는 유틸 클래스입니다. 인스턴스화를 할 수 없습니다.");
+    }
+
+}
