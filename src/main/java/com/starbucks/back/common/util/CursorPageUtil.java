ce5f0169e46e2c4ac14812fff662c3f7129c8daf
HyeonProG <167273902+HyeonProG@users.noreply.github.com>
2025-04-08 18:26:41 +0900

    feat: 기획전 페이징 처리 (#176)
    * chore: gitignore 추가
    
    * build: querydsl 추가 및 설정
    
    * feat: cursorpage util 설계
    
    * feat: querydsl config 설정
    
    * feat: pagingConstant 설계
    
    * feat: 기획전 페이징 처리

---
+++ src/main/java/com/starbucks/back/common/util/CursorPageUtil.java
@@ -0,0 +1,38 @@
+package com.starbucks.back.common.util;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class CursorPageUtil<T, C> {
+
+    private List<T> content;
+    private C nextCursor; // 제네릭 커서 타입
+    private Boolean hasNext;
+    private Integer pageSize;
+    private Integer page;
+
+    public boolean hasNext() {
+        return nextCursor != null;
+    }
+
+    @Builder
+    public CursorPageUtil(
+            List<T> content, // 현재 페이지 데이터 목록
+            C nextCursor, // 다음 페이지 조회용 커서
+            Boolean hasNext, // 다음 페이지 존재 여부
+            Integer pageSize, // 한 페이지당 데이터 개수
+            Integer page // 현재 페이지 번호
+    ) {
+        this.content = content;
+        this.nextCursor = nextCursor;
+        this.hasNext = hasNext;
+        this.pageSize = pageSize;
+        this.page = page;
+    }
+
+}
