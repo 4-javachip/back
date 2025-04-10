6c99d126f7be078cad411a7ccd478471fdaf6d2f
JungDongSeop <109326290+JungDongSeop@users.noreply.github.com>
2025-04-10 09:08:03 +0900

    refactor: 배송지 api 수정 (#196)
    * fix: 배송지 create api에 defaulted 항목 추가, 로직 수정
    
    * fix: 배송지 객체 조회 api에 defaulted 항목 추가
    
    * fix: 배송지 수정 api에 defaulted 추가, 로직 수정
    
    * refactor: 배송지 에러 처리 수정
    
    * refactor: 배송지 안쓰는 api 주석처리

--- src/main/java/com/starbucks/back/shippingaddress/infrastructure/ShippingAddressRepository.java
+++ src/main/java/com/starbucks/back/shippingaddress/infrastructure/ShippingAddressRepository.java
@@ -1,33 +1,44 @@
 package com.starbucks.back.shippingaddress.infrastructure;
 
 import com.starbucks.back.shippingaddress.domain.ShippingAddress;
+import com.starbucks.back.shippingaddress.dto.out.ResponseReadShippingAddressWithDefaultedDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ShippingAddressRepository extends JpaRepository<ShippingAddress, Long> {
 
     /**
-     * 배송지 조회 by shippingAddressUuid
+     * 배송지 조회 (defaulted 포함) by shippingAddressUuid
      * @param shippingAddressUuid
      * @return
      */
     Optional<ShippingAddress> findByShippingAddressUuid(String shippingAddressUuid);
 
-//    /**
-//     * 배송지테이블 전체 삭제 by userUuid
-//     * @param userUuid
-//     * @return
-//     */
-//    @Modifying
-//    @Query("""
-//        UPDATE ShippingAddress sa SET sa.deleted = true
-//        WHERE sa.shippingAddressUuid IN (
-//            SELECT usa.shippingAddressUuid
-//            FROM UserShippingAddress usa
-//            WHERE usa.userUuid = :userUuid AND usa.deleted = false
-//        )
-//        AND sa.deleted = false
-//    """)
-//    void bulkSoftDeleteShippingAddressesByUserUuid(@Param("userUuid") String userUuid);
+    @Query("""
+        SELECT new com.starbucks.back.shippingaddress.dto.out.ResponseReadShippingAddressWithDefaultedDto(
+            sa.id,
+            sa.shippingAddressUuid,
+            sa.addressName,
+            sa.recipientName,
+            sa.zipCode,
+            sa.baseAddress,
+            sa.detailAddress,
+            sa.phoneNumber,
+            sa.secondPhoneNumber,
+            sa.shippingNote,
+            usa.defaulted
+        )
+        FROM ShippingAddress sa
+        JOIN UserShippingAddress usa 
+            ON sa.shippingAddressUuid = usa.shippingAddressUuid
+        WHERE sa.shippingAddressUuid = :uuid AND usa.deleted = false
+    """)
+    Optional<ResponseReadShippingAddressWithDefaultedDto> findShippingAddressWithDefaultedByShippingAddressUuid(@Param("uuid") String uuid);
+
+
+
+
 }
